61a4c4937a8bc590043aaf5ecb8fb137985ccb25
Jonathan Williams <jhwgh2@mail.umsl.edu>
2015-04-11 19:00:46 -0500

    Fixed Try-catch

--- com/pset2/Mario.java
+++ com/pset2/Mario.java
@@ -5,35 +5,31 @@ import java.util.Scanner;
 public class Mario {
 
     public static void main(String[] args) {
+        int height = input();
+        Picks picker = new Picks(height);
+        picker.pick();
+    }
+
+    public static int input() {
         Scanner scanner = new Scanner(System.in);
         String size;
-        int height = 0;
+        int height;
         while (true) {
             try {
-                if (height < 0 || height > 23) {
-                    System.out.println("Invalid entry. Input must be in the form of numbers between 0 and 23 " +
-                            "inclusive.\nTry again.");
-                    System.out.print("How high is the pyramid? (between 0 and 23 inclusive): ");
-                    size = scanner.next();
-                } else {
-                    System.out.print("How high is the pyramid? (between 0 and 23 inclusive): ");
-                    size = scanner.next();
-                }
-                if (size.equals("0")) {
-                    System.out.println("Thank you! Have a nice day!");
-                    return;
-                }
+                System.out.print("How high is the pyramid? (between 0 and 23 inclusive): ");
+                size = scanner.next();
                 height = Integer.parseInt(size);
-                if (height > 0 && height < 24) {
-                    break;
-                }
             } catch (NumberFormatException ex) {
                 System.out.println("Invalid entry. Input must be in the form of numbers between 0 and 23 inclusive" +
                         ".\nTry again.");
-                height = 0;
+                continue;
+            }
+            if (height < 0 || height > 23) {
+                System.out.println("Invalid entry. Input must be in the form of numbers between 0 and 23 " +
+                        "inclusive.\nTry again.");
+            } else if (height >= 0 || height < 23) {
+                return height;
             }
         }
-        Picks picker = new Picks(height);
-        picker.pick();
     }
 }
